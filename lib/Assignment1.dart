a70a4e8613e30e3f2524a222c51155ad36e2afd8
hp <solankikashish2224@gmail.com>
2026-07-01 13:03:46 +0530

    assignment file create task.add comments.

--- lib/Assignment1.dart
+++ lib/Assignment1.dart
@@ -1,18 +1,28 @@
 void main()
 {
+  //print your full name
   print("Name:Solanki kashish kanubhai");
+  //print your address
   print("Address:Bhuval,hathijan");
+  //print your college name
   print("College Name:Government BCA college");
+  //print your hobbies
   print("Hobbies:dancing,singing,travelling");
+  //print your skills
   print("Skills:HTML,CSS,Python");
+  //print your mobile number
   print("Mobile No:9106022838");
+  //print today`s date
   print("Today`s date:30 jun 2026");
+  //print a welcome message
   print("Welcome");
+  //print a simple invoice
   print("=====Invoice=====");
   print("shop name:shreenath");
   print("item:laptop");
   print("price:50000");
   print("Brand=HP");
+  //print a 5 * 5 star pattern
   print("5 * 5 patter");
   print("*****");
   print("*****");
