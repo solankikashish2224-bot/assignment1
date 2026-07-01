91dfe17a5fd4acdeb67b91f12595b982a95ba8df
hp <solankikashish2224@gmail.com>
2026-07-01 12:53:35 +0530

    assignment file create task

---
+++ lib/Assignment1.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  print("Name:Solanki kashish kanubhai");
+  print("Address:Bhuval,hathijan");
+  print("College Name:Government BCA college");
+  print("Hobbies:dancing,singing,travelling");
+  print("Skills:HTML,CSS,Python");
+  print("Mobile No:9106022838");
+  print("Today`s date:30 jun 2026");
+  print("Welcome");
+  print("=====Invoice=====");
+  print("shop name:shreenath");
+  print("item:laptop");
+  print("price:50000");
+  print("Brand=HP");
+  print("5 * 5 patter");
+  print("*****");
+  print("*****");
+  print("*****");
+  print("*****");
+  print("*****");
+
+}
