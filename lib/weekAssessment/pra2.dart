45f31c0e9edca783460f91ce716248aa09c4e3aa
hp <solankikashish2224@gmail.com>
2026-07-04 13:13:58 +0530

    weekly  assesment.

--- lib/weekAssessment/pra2.dart
+++ lib/weekAssessment/pra2.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main()
 {
-stdout.write("Add Age:");
+  stdout.write("Add Age:");
 int age = int.parse(stdin.readLineSync()!);
 if(age >=18)
   {
@@ -11,4 +11,9 @@ else
   {
     print("validate Age:${age}");
   }
+List<int>numbers=[10,90,80,40,10,20];
+stdout.write("NUmbers:${numbers}");
+print("");
+Set uniq=numbers.toSet();
+stdout.write("NUmbers:${uniq}");
 }
